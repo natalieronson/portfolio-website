6d3db305bd3e51a80b7dbd1596a3b534a3580ab3
natalieronson <natalie.ronson@gmail.com>
2018-07-31 09:28:08 -0400

    Fixed navbar issue

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -6,4 +6,15 @@ $(document).ready(function () {
             scrollTop: $($.attr(this, 'href')).offset().top
         }, 1000);
     });
+
+    $(".dropdown").on("click", function() {
+        console.log("i'm clicked!")
+        // $(".header .wrapper ul li a").toggleClass("displayMenu");
+        // $(".header .wrapper ul").toggleClass("animateMenu");
+        $("nav ul li a").toggleClass("displayMenu");
+        $("nav ul li").toggleClass("hideLink");
+        $(".dropdown").toggleClass("hideLink");
+        $("nav ul").toggleClass("animateMenu");
+        $("nav ul").toggleClass("backgroundOpacity");
+    })
 });
